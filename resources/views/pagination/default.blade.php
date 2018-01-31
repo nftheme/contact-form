8651c31b30c627dffc5da5be93ee7170637df024
garung <canmotcaiten1993@gmail.com>
2018-01-31 16:41:24 +0700

    fix issue pagination

--- resources/views/pagination/default.blade.php
+++ resources/views/pagination/default.blade.php
@@ -5,7 +5,7 @@
         <li class="page-item"><a class="page-link" href="{{ $prev_page_url }}" rel="prev">&laquo;</a></li>
     @endif
     <li class="info-page-item">{{ $page_query_param . '/' . $total_page }}</li>
-    @if ($page_query_param >= $total)
+    @if ($page_query_param >= $total_page)
         <li class="page-item disabled"><span class="page-link">&raquo;</span></li>
     @else
         <li class="page-item"><a class="page-link" href="{{ $next_page_url }}" rel="next">&raquo;</a></li>
